1baf496949d0c97bea081a72ec2aa307e19be834
Pawfuqt <mendozabenedick78@gmail.com>
2025-08-13 02:30:25 -0700

    updated

--- src/app2c/App2c.java
+++ src/app2c/App2c.java
@@ -1,11 +1,28 @@
 
 package app2c;
 
+import java.util.Scanner;
+
 public class App2c {
 
     
     public static void main(String[] args) {
-     
+     Scanner sc =  new Scanner (System.in);
+         
+         int num;
+         System.out.print("Enter a Number: ");
+         num = sc.nextInt();
+         
+         
+         if (num == 0){
+             System.out.println("Inputed Number is Zero");
+         }else if (num < 0){
+             System.out.println(num+" is a Negative Number");
+         }
+         else
+         System.out.println(num+" is a Positive Number");
+    
+    }
     }
     
-}
+
